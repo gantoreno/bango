7796661cbebc9a0eec207a79e0e02014750fe882
Gabriel Moreno <gamoreno@urbe.edu.ve>
2020-07-31 23:26:27 -0400

    Fix favicon issues

--- views/components/links.php
+++ views/components/links.php
@@ -1,21 +1,13 @@
-<link rel="apple-touch-icon" sizes="57x57" href="/assets/ico/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/assets/ico/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/assets/ico/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/assets/ico/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/assets/ico/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/assets/ico/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/assets/ico/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/assets/ico/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/assets/ico/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="/assets/ico/android-icon-192x192.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/ico/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="/assets/ico/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="/assets/ico/favicon-96x96.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/assets/ico/favicon-16x16.png">
-<link rel="manifest" href="/public/manifest.json">
+<link rel="manifest" href="/assets/ico/site.webmanifest">
+<link rel="mask-icon" href="/assets/ico/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/assets/ico/favicon.ico">
 
 <link rel="stylesheet" href="/assets/css/main.css"  type="text/css" />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/assets/ico/ms-icon-144x144.png">
+<meta name="msapplication-TileColor" content="#b91d47">
+<meta name="msapplication-config" content="/assets/ico/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
